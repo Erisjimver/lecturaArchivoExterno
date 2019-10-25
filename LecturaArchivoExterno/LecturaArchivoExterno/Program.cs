2ea0a2242cc4eb61794542fff640194a774cf6c6
erisjimver <erisjinver@gmail.com>
2019-10-25 00:02:46 -0500

    First Commit

---
+++ LecturaArchivoExterno/LecturaArchivoExterno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LecturaArchivoExterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.IO.StreamReader archivo = null;
+
+            try
+            {
+                string linea;
+                int contador = 0;
+                string patch = "";
+                archivo = new System.IO.StreamReader(patch);
+                while ((linea = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error con la lectura del archivo");
+            }
+            finally
+            {if (archivo != null) archivo.Close();
+                Console.WriteLine("Cerrando la conexion de la lectura");
+
+            }
+        }
+    }
+}
